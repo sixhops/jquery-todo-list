3d381bfd91e33ecaba911d3320c731ee163c9848
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-09-05 20:47:44 -0700

    Basic solution finished

--- js/main.js
+++ js/main.js
@@ -1,1 +1,21 @@
 console.log('Scripts running...');
+
+$(document).ready(function() {
+  console.log("DOM loaded...");
+
+  $('[name=addbutton]').on('click', function() {
+    // get the name of the task from the textbox
+    var itemText = $('[name=itemtoadd]').val();
+    // Create a li with that text
+    var item = $('<li>').html("<span class='delete'>x</span> " + itemText);
+    $('ul').append(item);
+    $('[name=itemtoadd]').val('');
+    $('[name=itemtoadd]').focus();
+  });
+
+  $('ul').on('click', 'span', function(e) {
+    $(e.target).parent().remove();
+  });
+
+});
+
