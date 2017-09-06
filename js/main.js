6e5d91ca1675f5543cfd7d1e8916ba850c8a7038
Steven Peters <steve@Jennas-MacBook-Air.local>
2017-09-05 21:59:38 -0700

    Adding comments and cleaning up

--- js/main.js
+++ js/main.js
@@ -6,16 +6,19 @@ $(document).ready(function() {
   $('[name=addbutton]').on('click', function() {
     // get the name of the task from the textbox
     var itemText = $('[name=itemtoadd]').val();
-    // Create a li with that text
+    // Create a li with a span and that text
     var item = $('<li>').html("<span class='delete'>x</span> " + itemText);
+    // Append the li to the ul
     $('ul').append(item);
+    // Clear the input textbox
     $('[name=itemtoadd]').val('');
+    // Set forcus on the textbox
     $('[name=itemtoadd]').focus();
   });
 
   $('ul').on('click', 'span', function(e) {
+    // User clicked on the span - remove the span's parent
     $(e.target).parent().remove();
   });
 
 });
-
